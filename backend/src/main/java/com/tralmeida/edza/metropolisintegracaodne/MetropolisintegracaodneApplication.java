8800b814b1a7f52abd3c7a24e3ac09b958ec343d
Thierry Almeida <thierryluiz@hotmail.com>
2023-09-28 20:04:23 -0300

    First commit

---
+++ backend/src/main/java/com/tralmeida/edza/metropolisintegracaodne/MetropolisintegracaodneApplication.java
@@ -0,0 +1,13 @@
+package com.tralmeida.edza.metropolisintegracaodne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetropolisintegracaodneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetropolisintegracaodneApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tralmeida/edza/metropolisintegracaodne/MetropolisintegracaodneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tralmeida.edza.metropolisintegracaodne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetropolisintegracaodneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
